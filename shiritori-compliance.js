a797bebe71465146392231d9caa4988a7d4142dc
Raffaele Procentese <deadpikasso@gmail.com>
2016-05-27 17:53:55 +0200

    minor bugfix, added long vowel check

--- shiritori-compliance.js
+++ shiritori-compliance.js
@@ -1,18 +1,24 @@
 var rules = {
-  nNotALosingKana: true,
+  longVowelFallbackToPreviousKanaIsValid: true,
+  allowNKana: true,
   smallKanaFallbackToPreviousKanaIsValid: true
 };
+
+console.log(areWordsShiritoriCompliant(bef, aft));
 
 function areWordsShiritoriCompliant (bef, aft) {
   var befHira = rmDakutenAndCapsSmallVowels(convertToHiragana(bef));
   var aftHira = rmDakutenAndCapsSmallVowels(convertToHiragana(aft));
 
   var isSmallKana = ['ゃ', 'ゅ', 'ょ'].indexOf(befHira[befHira.length - 1]) !== -1;
+  var isLongVowel = 'ー' === befHira[befHira.length - 1];
   var endsInNG = 'ん' === befHira[befHira.length - 1];
 
   if (befHira.charCodeAt(befHira.length - 1) === aftHira.charCodeAt(0) ||
-    (smallKanaFallbackToPreviousKanaIsValid && isSmallKana && befHira.charCodeAt(befHira.length - 2) === aftHira.charCodeAt(0)) ||
-    (nNotALosingKana && endsInNG && ['ん', 'な', 'に', 'ぬ', 'ね', 'の'].indexOf(aftHira(0)))) {
+    (isLongVowel && convertLongVowelSignToVowel(befHira[befHira.length - 2]).charCodeAt(0) === aftHira.charCodeAt(0)) ||
+    (rules.smallKanaFallbackToPreviousKanaIsValid && isSmallKana && befHira.charCodeAt(befHira.length - 2) === aftHira.charCodeAt(0) && befHira.charCodeAt(befHira.length - 1) === aftHira.charCodeAt(1)) ||
+    (rules.longVowelFallbackToPreviousKanaIsValid && isLongVowel && befHira.charCodeAt(befHira.length - 2) === aftHira.charCodeAt(0)) ||
+    (rules.allowNKana && endsInNG && ['ん', 'な', 'に', 'ぬ', 'ね', 'の'].indexOf(aftHira(0)))) {
     return true;
   }
 
@@ -36,8 +42,7 @@ function rmDakutenAndCapsSmallVowels (word) {
     ['か', 'が'], ['き', 'ぎ'], ['く', 'ぐ'], ['け', 'げ'], ['こ', 'ご'], ['さ', 'ざ'], ['し', 'じ'],
     ['す', 'ず'], ['せ', 'ぜ'], ['そ', 'ぞ'], ['た', 'だ'], ['ち', 'ぢ'], ['つ', 'っ', 'づ'], ['て', 'で'],
     ['と', 'ど'], ['は', 'ば', 'ぱ'], ['ひ', 'び', 'ぴ'], ['ふ', 'ぶ', 'ぷ'], ['へ', 'べ', 'ぺ'], ['ほ', 'ぼ', 'ぽ'],
-    ['う', 'ゔ']
-  ];
+    ['う', 'ゔ'] ];
   var convertedWord = '';
   for (i in word) {
     var kana = word[i];
@@ -46,6 +51,22 @@ function rmDakutenAndCapsSmallVowels (word) {
     });
     convertedWord += idx !== -1 ? dakutenMap[idx][0] : word[i];
   }
+
+  return convertedWord;
+}
+
+function convertLongVowelSignToVowel (beforeSign) {
+  var hiraTable = [ ['あ', 'か', 'さ', 'た', 'な', 'は', 'ま', 'や', 'ら', 'わ'],
+    ['い', 'き', 'し', 'ち', 'に', 'ひ', 'み', 'り'],
+    ['う', 'く', 'す', 'つ', 'ぬ', 'ふ', 'む', 'ゆ'],
+    ['え', 'け', 'せ', 'て', 'ね', 'へ', 'め', 'れ'],
+    ['お', 'こ', 'そ', 'と', 'の', 'ほ', 'も', 'よ', 'ろ'] ];
+
+  var convertedWord = '';
+  var idx = hiraTable.findIndex((item, idx) => {
+    return item.indexOf(beforeSign) !== -1;
+  });
+  convertedWord += idx !== -1 ? hiraTable[idx][0] : beforeSign;
 
   return convertedWord;
 }
